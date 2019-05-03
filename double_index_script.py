874dad6698cab330849db7989e7e6636891e8942
John Lawless <42017593+LawlessJ@users.noreply.github.com>
2019-05-03 14:23:53 -0400

    Adds examples to print to console

--- double_index_script.py
+++ double_index_script.py
@@ -6,5 +6,7 @@ def double_index(lst, index):
     return lst
 
 
-#Uncomment the line below when your function is done
 print(double_index([3, 8, -10, 12], 2))
+# returns [3, 8, -20, 12] to the console
+print(double_index([3, 8, -10, 12], -1))
+# returns [3, 8, -10, 24] to the console
